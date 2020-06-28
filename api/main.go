d62ed96bd3c645b7d644dbc0018a762cd2bd998f
tanaka.takurou <tanaka.takurou@icloud.com>
2020-06-28 18:31:10 +0900

    initial

---
+++ api/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"context"
+	"encoding/json"
+	"encoding/base64"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/textract"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type APIResponse struct {
+	Message  string `json:"message"`
+}
+
+type Response events.APIGatewayProxyResponse
+
+const layout string = "2006-01-02 15:04"
+
+func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (Response, error) {
+	var jsonBytes []byte
+	var err error
+	d := make(map[string]string)
+	json.Unmarshal([]byte(request.Body), &d)
+	if v, ok := d["action"]; ok {
+		switch v {
+		case "analyzedocument" :
+			if i, ok := d["image"]; ok {
+				r, e := analyzeDocument(i)
+				if e != nil {
+					err = e
+				} else {
+					jsonBytes, _ = json.Marshal(APIResponse{Message: r})
+				}
+			}
+		}
+	}
+	log.Print(request.RequestContext.Identity.SourceIP)
+	if err != nil {
+		log.Print(err)
+		jsonBytes, _ = json.Marshal(APIResponse{Message: fmt.Sprint(err)})
+		return Response{
+			StatusCode: 500,
+			Body: string(jsonBytes),
+		}, nil
+	}
+	return Response {
+		StatusCode: 200,
+		Body: string(jsonBytes),
+	}, nil
+}
+
+func analyzeDocument(img string)(string, error) {
+	b64data := img[strings.IndexByte(img, ',')+1:]
+	data, err := base64.StdEncoding.DecodeString(b64data)
+	if err != nil {
+		log.Print(err)
+		return "", err
+	}
+	svc := textract.New(session.New(), &aws.Config{
+		Region: aws.String("us-west-2"),
+	})
+
+	input := &textract.AnalyzeDocumentInput{
+		Document: &textract.Document{
+			Bytes: data,
+		},
+		FeatureTypes: []*string{aws.String("TABLES")},
+	}
+	res, err2 := svc.AnalyzeDocument(input)
+	if err2 != nil {
+		return "", err2
+	}
+	if len(res.Blocks) < 1 {
+		return "No Document", nil
+	}
+	var wordList []string
+	for _, v := range res.Blocks {
+		if aws.StringValue(v.BlockType) == "WORD" || aws.StringValue(v.BlockType) == "LINE" {
+			wordList = append(wordList, aws.StringValue(v.Text))
+		}
+	}
+	results, err3 := json.Marshal(wordList)
+	if err3 != nil {
+		return "", err3
+	}
+	return string(results), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
---
+++ static/js/main.js
@@ -0,0 +1,64 @@
+var ChangeImage = function() {
+  const file = $('#image').prop('files')[0];
+  toBase64(file).then(onConverted());
+}
+
+var toBase64 = function(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+  });
+}
+
+var onConverted = function() {
+  return function(v) {
+    App.imgdata = v;
+    $('#preview').attr('src', v);
+  }
+}
+
+var SubmitForm = function() {
+  if (!App.imgdata) {
+    $("#warning").text("Image is Empty").removeClass("hidden").addClass("visible");
+    return false;
+  }
+  $("#submit").addClass('disabled');
+  var action  = $("#action").val();
+  var image = App.imgdata;
+  const data = {action, image};
+  request(data, (res)=>{
+    $("#result").text(res.message);
+    $("#info").removeClass("hidden").addClass("visible");
+    ScrollBottom();
+  }, (e)=>{
+    console.log(e.responseJSON.message);
+    $("#warning").text(e.responseJSON.message).removeClass("hidden").addClass("visible");
+    $("#submit").removeClass('disabled');
+  });
+};
+
+var request = function(data, callback, onerror) {
+  $.ajax({
+    type:          'POST',
+    dataType:      'json',
+    contentType:   'application/json',
+    scriptCharset: 'utf-8',
+    data:          JSON.stringify(data),
+    url:           {{ .Api }}
+  })
+  .done(function(res) {
+    callback(res);
+  })
+  .fail(function(e) {
+    onerror(e);
+  });
+};
+
+var ScrollBottom = function() {
+  var bottom = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  window.scroll(0, bottom);
+}
+
+var App = { token: '', imgdata: null };
